a745dc2bf284dca6a553d503034329d266cc0c69
mohamednassirouahamadou9696@gmail.com <mohamednassirouahamadou9696@gmail.com>
2023-02-19 23:27:54 +0100

    Rush01

---
+++ printBox.js
@@ -0,0 +1,21 @@
+function printBox() {
+  var box = {
+    width1:  "******",
+    height1: "*    *",
+    height2: "*    *",
+    width2:  "******",
+  
+    printBox: function(height, width) {
+      return this.width1 +
+        '\n' + this.height1 +
+        '\n' + this.height2 +
+        '\n' + this.width2;
+  
+    }
+  
+  }
+  
+  console.log(box.printBox());
+}
+printBox();
+module.exports=printBox
---
+++ treeConstructor.js
@@ -0,0 +1,11 @@
+function TreeConstructor(strArr) {
+  var strArr = prompt() ;
+  if(true) {
+    console.log(true);
+  } else {
+    console.log(false);
+  }
+}
+TreeConstructor(["(1,2)", "(2,4)", "(5,7)", "(7,2)", "(9,5)"]);
+
+module.exports =treeConstructor
